f9c753bed7f9a30e0572b4de420869fc99010905
Franklin Antony <franklinanton.007@gmail.com>
2018-05-24 00:37:48 +0530

    Corrected Bubble Sort

--- Sorting/Bubble_Sort.go
+++ Sorting/Bubble_Sort.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 )
 func bubbleSort(array_input []int){
-
+	swapped := true;
+	for swapped {
+		swapped = false
+		for i := 0; i < len(array_input) - 1; i++ {
+			if array_input[i + 1] < array_input[i] {
+				Swap(array_input, i, i + 1)
+				swapped = true
+			}
+		}
+	}	
 }
-
-func main ()
-{
+func Swap(array_input []int, i, j int) {
+	tmp := array_input[j]
+	array_input[j] = array_input[i]
+	array_input[i] = tmp
+}
+func main (){
 
 	array_input := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
 	fmt.Println("Unsorted array: ", array_input)
