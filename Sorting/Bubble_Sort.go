be7853096ce66ccc94bfb8839d63912570d40033
Franklin Antony <franklinanton.007@gmail.com>
2018-05-24 00:34:32 +0530

    Updated Bubble Sort

--- Sorting/Bubble_Sort.go
+++ Sorting/Bubble_Sort.go
@@ -1,4 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+func bubbleSort(array_input []int){
+
+}
+
 func main ()
 {
-	fmt.Println("Hello World")
+
+	array_input := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
+	fmt.Println("Unsorted array: ", array_input)
+	bubbleSort(array_input)
+	fmt.Println("Sorted array: ", array_input)
+	
 }
