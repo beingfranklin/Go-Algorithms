ef9370b72769236ef6d1feb11b5b5db305b8e752
Franklin Antony <franklinanton.007@gmail.com>
2018-06-04 10:56:29 +0530

    Updated Typos

--- Sorting/Bubble_Sort.go
+++ Sorting/Bubble_Sort.go
@@ -3,28 +3,29 @@ package main
 import (
 	"fmt"
 )
-func bubbleSort(array_input []int){
-	swapped := true;
+
+func bubbleSort(array_input []int) {
+	swapped := true
 	for swapped {
 		swapped = false
-		for i := 0; i < len(array_input) - 1; i++ {
-			if array_input[i + 1] < array_input[i] {
-				Swap(array_input, i, i + 1)
+		for i := 0; i < len(array_input)-1; i++ {
+			if array_input[i+1] < array_input[i] {
+				Swap(array_input, i, i+1)
 				swapped = true
 			}
 		}
-	}	
+	}
 }
 func Swap(array_input []int, i, j int) {
 	tmp := array_input[j]
 	array_input[j] = array_input[i]
 	array_input[i] = tmp
 }
-func main (){
+func main() {
 
 	array_input := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
 	fmt.Println("Unsorted array: ", array_input)
 	bubbleSort(array_input)
 	fmt.Println("Sorted array: ", array_input)
-	
+
 }
