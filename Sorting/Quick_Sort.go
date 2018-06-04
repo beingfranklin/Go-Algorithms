faf9889a251d0e6a418ada70a3d2ee5a2c6adad7
Franklin Antony <franklinanton.007@gmail.com>
2018-06-04 10:58:19 +0530

    Added Quick Sort

---
+++ Sorting/Quick_Sort.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func quick_sort(arr []int) []int {
+
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	median := arr[rand.Intn(len(arr))]
+
+	low_part := make([]int, 0, len(arr))
+	high_part := make([]int, 0, len(arr))
+	middle_part := make([]int, 0, len(arr))
+
+	for _, item := range arr {
+		switch {
+		case item < median:
+			low_part = append(low_part, item)
+		case item == median:
+			middle_part = append(middle_part, item)
+		case item > median:
+			high_part = append(high_part, item)
+		}
+	}
+
+	low_part = quick_sort(low_part)
+	high_part = quick_sort(high_part)
+
+	low_part = append(low_part, middle_part...)
+	low_part = append(low_part, high_part...)
+
+	return low_part
+}
+
+func main() {
+	arr := []int{1, 6, 2, 4, 9, 0, 5, 3, 7, 8}
+	fmt.Println("Initial array is:", arr)
+	fmt.Println("")
+	fmt.Println("Sorted array is: ", quick_sort(arr))
+}
